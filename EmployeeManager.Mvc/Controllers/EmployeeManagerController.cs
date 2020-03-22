3670b553741d8ae0d97c5e09f56f3314e850861c
Viktar Radzin <viktarradzin@outlook.com>
2020-03-22 13:04:20 +0300

    01 commit

---
+++ EmployeeManager.Mvc/Controllers/EmployeeManagerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManager.Mvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeManager.Mvc.Controllers
+{
+    public class EmployeeManagerController : Controller
+    {
+        private AppDbContext db = null;
+        public EmployeeManagerController(AppDbContext db)
+        {
+            this.db = db;
+        }
+
+        private void FillCountries()
+        {
+            List<SelectListItem> countries =
+            (from c in db.Countries
+             orderby c.Name ascending
+             select new SelectListItem()
+             {
+                 Text = c.Name,
+                 Value = c.Name
+             }).ToList();
+            ViewBag.Countries = countries;
+        }
+    }
+}
