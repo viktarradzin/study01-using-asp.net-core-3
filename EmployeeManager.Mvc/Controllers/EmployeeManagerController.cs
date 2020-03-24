6a82354ce7d2e7d453cf5fa6e3b79d1afc42e839
Viktar Radzin <viktarradzin@outlook.com>
2020-03-24 18:59:53 +0300

    03 commit

--- EmployeeManager.Mvc/Controllers/EmployeeManagerController.cs
+++ EmployeeManager.Mvc/Controllers/EmployeeManagerController.cs
@@ -57,5 +57,41 @@ namespace EmployeeManager.Mvc.Controllers
             }
             return View(model);
         }
+
+        public IActionResult Update(int id)
+        {
+            FillCountries();
+            Employee model = db.Employees.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Employee model)
+        {
+            FillCountries();
+            if (ModelState.IsValid)
+            {
+                db.Employees.Update(model);
+                db.SaveChanges();
+                ViewBag.Message = "Employee updated successfully";
+            }
+            return View(model);
+        }
+
+        [ActionName("Delete")]
+        public IActionResult ConfirmDelete(int id)
+        {
+            Employee model = db.Employees.Find(id);
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult Delete(int employeeID)
+        {
+            Employee model = db.Employees.Find(employeeID);
+            db.Employees.Remove(model);
+            db.SaveChanges();
+            TempData["Message"] = "Employee deleted successfully";
+            return RedirectToAction("List");
+        }
     }
 }
--- EmployeeManager.Mvc/Startup.cs
+++ EmployeeManager.Mvc/Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManager.Mvc.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,10 +15,19 @@ namespace EmployeeManager.Mvc
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private IConfiguration config = null;
+
+
+        public Startup(IConfiguration config)
+        {
+            this.config = config;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(
+            options => options.UseSqlServer
+            (this.config.GetConnectionString("AppDb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +37,16 @@ namespace EmployeeManager.Mvc
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseStaticFiles();
             app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllerRoute(
+                 name: "default",
+                 pattern: "{controller=EmployeeManager}/{action=List}/{id?}");
             });
         }
+
+
     }
+    
 }
