0cb1cb9744969c4346b4aa0332e78ba2c6efde08
Viktar Radzin <viktarradzin@outlook.com>
2020-03-24 20:43:13 +0300

    Commit 04

--- EmployeeManager.Mvc/Startup.cs
+++ EmployeeManager.Mvc/Startup.cs
@@ -2,14 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using EmployeeManager.Mvc.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using EmployeeManager.Mvc.Models;
+using EmployeeManager.Mvc.Security;
+using Microsoft.EntityFrameworkCore;
 
 namespace EmployeeManager.Mvc
 {
@@ -25,9 +26,20 @@ namespace EmployeeManager.Mvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<AppDbContext>(
-            options => options.UseSqlServer
-            (this.config.GetConnectionString("AppDb")));
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(this.config.GetConnectionString("AppDb")));
+
+            services.AddDbContext<AppIdentityDbContext>(options =>
+        options.UseSqlServer(this.config.GetConnectionString("AppDb")));
+
+            services.AddIdentity<AppIdentityUser, AppIdentityRole>()
+                    .AddEntityFrameworkStores<AppIdentityDbContext>();
+
+            services.ConfigureApplicationCookie(opt =>
+            {
+                opt.LoginPath = "/Security/SignIn";
+                opt.AccessDeniedPath = "/Security/AccessDenied";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +51,8 @@ namespace EmployeeManager.Mvc
             }
             app.UseStaticFiles();
             app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllerRoute(
                  name: "default",
